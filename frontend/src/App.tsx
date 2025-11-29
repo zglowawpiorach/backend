5d684ca7d7844896a506e2d9025986953bff4252
Seba <sebsym96@gmail.com>
2025-11-29 18:42:23 +0100

    fixing eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="bg-amber-950 w-screen h-screen">piorka</div>
